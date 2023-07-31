091cc805820b2a96f172af33a54f4ba0f88658ea
Rangel Cuesta <rangelus87@gmail.com>
2023-07-31 13:21:01 -0300

    refact conversor de temperatura

--- Conversor-jOptionPane/src/Test/Test.java
+++ Conversor-jOptionPane/src/Test/Test.java
@@ -7,11 +7,11 @@ import Views.Input;
 import Views.MenuPrincipal;
 
 public class Test {
-    
+
     private void llamarConvertidor(int i) {
         Input input = new Input();
         float ValorIngresado = input.getEntrada();
-        
+
         if (i == 1) {
             ConvesorMonedas convesorMonedas = new ConvesorMonedas();
             convesorMonedas.obtenerSeleccion(ValorIngresado);
@@ -19,9 +19,15 @@ public class Test {
             ConversorMedidas conversorMedidas = new ConversorMedidas();
         } else if (i == 3) {
             ConversorTemperatura conversorTemperatura = new ConversorTemperatura();
+            conversorTemperatura.obtenerSeleccion(ValorIngresado);
+            System.out.println(conversorTemperatura.getValorConvertido());
         }
     }
-    
+
+    private void valorConversion(int conversor, float valor) {
+
+    }
+
     public static void main(String[] args) {
         MenuPrincipal menuPrincipal = new MenuPrincipal();
         int seleccion = menuPrincipal.obtenerSeleccion();
--- Conversor-jOptionPane/src/Views/ConversorTemperatura.java
+++ Conversor-jOptionPane/src/Views/ConversorTemperatura.java
@@ -4,25 +4,57 @@ import javax.swing.JOptionPane;
 
 public class ConversorTemperatura {
 
-    private Object[] opcionesTemperaturas;
+    private enum TiposConversion {
+        SELECCIONE_UNA_OPCION,
+        CELSIUS_A_FAHRENHEIT,
+        CELSIUS_A_KELVIN,
+        FAHRENHEIT_A_CELSIUS,
+        FAHRENHEIT_A_KELVIN,
+        KELVIN_A_CELSIUS,
+        KELVIN_A_FAHRENHEIT;
+    }
+
+    private TiposConversion seleccionConversion;
+    private float valorConvertido;
 
     public ConversorTemperatura() {
-        opcionesTemperaturas = new Object[]{
-            "Seleccione una Opcion",
-            "Celsius a Fahrenheit",
-            "Celsius a Kelvin",
-            "Fahrenheit a Celsius",
-            "Fahrenheit a Kelvin",
-            "Kelvin a Celsius",
-            "Kelvin a Fahrenheit"
-        };
-        Object seleccionar = JOptionPane.showInputDialog(
+        TiposConversion[] tiposConversion = TiposConversion.values();
+        seleccionConversion = (TiposConversion) JOptionPane.showInputDialog(
                 null,
-                "Tipo de conversion: ",
+                "Seleccione el tipo de conversion",
                 "Temperatura",
                 JOptionPane.QUESTION_MESSAGE,
-                null, opcionesTemperaturas,
-                opcionesTemperaturas[0]
+                null,
+                tiposConversion,
+                tiposConversion[0]
         );
+    }
+
+    public void obtenerSeleccion(float num) {
+
+        switch (seleccionConversion) {
+            case CELSIUS_A_FAHRENHEIT:
+                valorConvertido = (num * 1.8F) + 32;
+                break;
+            case CELSIUS_A_KELVIN:
+                valorConvertido = num + 273.15F;
+                break;
+            case FAHRENHEIT_A_CELSIUS:
+                valorConvertido = (num - 32) / 1.8F;
+                break;
+            case FAHRENHEIT_A_KELVIN:
+                valorConvertido = ((num - 32) / 1.8F) + 273.15F;
+                break;
+            case KELVIN_A_CELSIUS:
+                valorConvertido = num - 273.15F;
+                break;
+            case KELVIN_A_FAHRENHEIT:
+                valorConvertido = ((num - 273.15F) * 1.8F) + 32;
+                break;
+        }
+    }
+
+    public float getValorConvertido() {
+        return valorConvertido;
     }
 }
